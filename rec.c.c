307e96371ec96f0af858c751500db7465752e9e4
BRAVON254 <bravonshelkh@gmail.com>
2024-09-12 13:20:01 +0300

    Add files via upload

---
+++ rec.c.c
@@ -0,0 +1,11 @@
+// Area of a rectangle
+#include <stdio.h>
+int main() {
+int length,width,mult;
+printf("enter length");
+scanf("%d",&length);
+printf("enter width");
+scanf("%d",&width);
+mult=length*width;
+printf("the area of the rectangle is %d",mult);
+}

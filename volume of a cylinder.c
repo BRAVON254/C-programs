8a9914c6bb6847f825f800d2f1c48e1094a878e4
BRAVON254 <bravonshelkh@gmail.com>
2024-09-23 10:04:49 +0300

    Add files via upload

---
+++ volume of a cylinder.c
@@ -0,0 +1,13 @@
+// volume of a cylinder 
+#include<stdio.h>
+int main(){
+// declaration and initialization
+float r,h,vol,pie=3.14;
+printf("enter r");
+scanf("%f",&r);
+printf("enter h");
+scanf("%f",&h);
+vol=pie*r*r*h;
+printf("volume of the cylinder is : %f",vol);
+
+}
